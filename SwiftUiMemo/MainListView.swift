6322c4b5eaf68d3a08fb43daa1f725c818963b4a
seungbeom <chsb1221@naver.com>
2023-07-31 18:10:31 +0900

    Step3

--- SwiftUiMemo/MainListView.swift
+++ SwiftUiMemo/MainListView.swift
@@ -8,13 +8,31 @@
 import SwiftUI
 
 struct MainListView: View {
+    @EnvironmentObject var store: MemoStore
+   // @EnvironmentObject
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView{
+            List(store.list)
+            {
+                memo in VStack(alignment: .leading) {
+                    Text(memo.content)
+                        .font(.body)
+                        .lineLimit(1)
+                    
+                    Text(memo.insertDate, style: .date)
+                        .font(.caption)
+                        .foregroundColor(.secondary)
+                }
+        }
+            .listStyle(.plain)// 라운드진걸 없애줌
+            .navigationTitle("내 메모")
+        }
     }
 }
 
 struct MainListView_Previews: PreviewProvider {
     static var previews: some View {
         MainListView()
+            .environmentObject(MemoStore())
     }
 }
