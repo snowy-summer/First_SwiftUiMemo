a3ba82ba2743c59f3570a887da8bd1c6ff7284d6
seungbeom <chsb1221@naver.com>
2023-07-31 15:25:52 +0900

    Initial Commit

---
+++ SwiftUiMemo/SwiftUiMemoApp.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftUiMemoApp.swift
+//  SwiftUiMemo
+//
+//  Created by 최승범 on 2023/07/31.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUiMemoApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
