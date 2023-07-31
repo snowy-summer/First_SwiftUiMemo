b5a5d04975752cd439115db624aff9bf7c511ad8
seungbeom <chsb1221@naver.com>
2023-07-31 15:30:45 +0900

    Step1

---
+++ SwiftUiMemo/MainListView.swift
@@ -0,0 +1,20 @@
+//
+//  MainListView.swift
+//  SwiftUiMemo
+//
+//  Created by 최승범 on 2023/07/31.
+//
+
+import SwiftUI
+
+struct MainListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MainListView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainListView()
+    }
+}
